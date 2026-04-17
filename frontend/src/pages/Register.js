5f8e8b4bd2c6d8348a984476efaf20975b30e66b
rithumithra <161004146+rithumithra@users.noreply.github.com>
2026-04-17 23:12:53 +0530

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -23,7 +23,7 @@ export default function Register() {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/register", {
+      const res = await fetch("https://vytrix-backend.onrender.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
