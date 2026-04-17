114f198159a8260e68530acfb54fd040ec729674
rithumithra <161004146+rithumithra@users.noreply.github.com>
2026-04-17 23:13:41 +0530

    Update RegisterAccount.js

--- frontend/src/pages/RegisterAccount.js
+++ frontend/src/pages/RegisterAccount.js
@@ -15,7 +15,7 @@ export default function RegisterAccount() {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/send_otp", {
+      const res = await fetch("https://vytrix-backend.onrender.com/send_otp", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
